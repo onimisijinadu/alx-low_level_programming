81bf102bbae818c727482b91d75caa9e8059dec1
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-17 04:25:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Retun: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
