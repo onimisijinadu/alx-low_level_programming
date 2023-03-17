670d24bfca0105a6e144aa087061b5cc4156c382
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-17 13:48:55 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %c", c);
+	printf("Size of an int: %d", i);
+	printf("Size of a long int: %ld", li);
+	printf("Size of a long long int: %lf", ll);
+	printf("Size of a float: %f", f);
+
+	return (0);
+}
+
