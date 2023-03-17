779227d8b9eb4e4aa0ad4614f17becbcbb11884c
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-17 04:11:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * Write a C program that prints exactly
- * Programming is like building a multilingual puzzle,
- * followed by a new line.
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
