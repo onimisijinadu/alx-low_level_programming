8d3e71b52b36dd58ce08e1c7d15f3d16ede9e94f
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-17 04:08:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle", "\n");
+#include <stdio.h>
+
+/**
+ * Write a C program that prints exactly
+ * Programming is like building a multilingual puzzle,
+ * followed by a new line.
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
