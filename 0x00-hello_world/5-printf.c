8fbe0fb167721aae132946ed9a13a5429ce74929
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-17 04:59:40 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,13 @@
 
 /**
  * main = Entry point
- * -Wall gcc
  *
  *  Return: Always 0 (Sucess)
  */
 
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
