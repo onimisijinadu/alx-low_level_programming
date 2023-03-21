05fe37867de562abe9769ebcd54ba7ad2eca174a
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-21 06:58:57 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ *
+ * using putchar inplace of printf
+ */
+
+int main(void)
+{
+	char c;
+
+	for
+		(c = 'a'; c <= 'z'; c++)
+			putchar(c);
+
+	return (0);
+}
