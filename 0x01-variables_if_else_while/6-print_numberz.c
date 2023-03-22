19281b22519155ed7721f9bed47290b2eb80b82f
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-22 02:57:16 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int base;
+
+	for
+		(base = 0;
+		base <= 9;
+		base++)
+			putchar(base);
+	putchar('\n');
+
+	return (0);
+}
+
