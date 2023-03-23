e2a412328e4930b9c297c8b0a115e43444d37096
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-23 01:28:23 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for
+		(i = '0';
+		 i <= '9';
+		 ++i)
+			putchar(i);
+	putchar(',');
+
+	putchar('\n');
+
+	return (0);
+}
+
