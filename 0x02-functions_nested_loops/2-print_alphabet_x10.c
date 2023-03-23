5bd7e3a1dfab7b4420b63859b5e3d84da4beec65
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-23 07:27:02 -0700

    void print_alphabet_x10(void)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - void print_alphabet_x10(void);
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	if (c <=10)
+		_putchar(c);
+	c++;
+	for
+		(c = 'a';
+		 c <= 'z';
+		 c++)
+			_putchar(c);
+	putchar('\n');
+	return (0);
+}
+
