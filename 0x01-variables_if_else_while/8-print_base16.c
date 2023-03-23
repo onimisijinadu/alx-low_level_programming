fe4f6283ca13f9ffd6c66e4f8c09a8e5ee4f347b
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-23 01:12:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	char a;
+
+	for
+		(i = 0;
+		 i <= 9;
+		 ++i)
+			putchar(i);
+	for
+		(a = 'a';
+		 a <= 'f';
+		 a++)
+			putchar(a);
+	putchar('\n');
+	return (0);
+}
+
