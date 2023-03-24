7b5ea95e59ac307799dfcd9067a836d9fb8a67b3
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-24 02:47:31 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - functions for checking absolute value of integar
+ *
+ * @ab: integar value
+ *
+ * Return: return absolute value
+ */
+
+int _abs(int ab)
+{
+
+	return (ab * ((ab > 0) - (ab < 0)));
+}
