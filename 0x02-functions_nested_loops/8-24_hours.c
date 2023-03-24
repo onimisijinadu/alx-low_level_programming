d515b60c8f181b6761dfb1c82e250b7706df84db
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-24 03:44:18 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function for printing every minutes of the day
+ *
+ * h = hour, m = minutes
+ *
+ * Return: 24 hours clock
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + 0);
+			_putchar((h % 10) + 0);
+			_putchar(':');
+			_putchar((m / 10) + 0);
+			_putchar((m % 10) + 0);
+			_putchar('\n');
+		}
+	}
+}
