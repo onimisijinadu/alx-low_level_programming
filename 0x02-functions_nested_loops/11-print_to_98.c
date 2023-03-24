59414acb584c80487bc0529287f712126ee634ae
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-24 04:50:59 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - function for printing number to 98
+ *
+ * @n: input number
+ *
+ * Return:
+ */
+
+void print_to_98(int n)
+{
+
+
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+			if (n > 98)
+			{
+				while (n >= 98)
+				{
+					printf("%d", n);
+					if (n != 98)
+					{
+						printf(", ");
+					}
+					n--;
+				}
+			}
+			else
+			{
+				printf("98");
+			}
+			printf("/n");
+		}
+	}
+}
