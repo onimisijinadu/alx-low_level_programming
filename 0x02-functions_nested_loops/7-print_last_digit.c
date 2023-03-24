6644c50a47388be75a0fb389339c0631fdb23cb7
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-24 03:04:38 -0700

    7-print_last_digits.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function for checking last digit of a number
+ *
+ * @i: function for last digit
+ *
+ * Return: return value of last digit
+ */
+
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
