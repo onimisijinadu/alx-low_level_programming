caeb52be547d374f8984f8bfb01ed08edd40ef65
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-24 04:18:47 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - functions for adding numbers
+ *
+ * num1: first input
+ *
+ * num2: second input
+ *
+ * Return: Result
+ */
+
+int add(int num1, int num2)
+{
+	int add = num1 + num2;
+
+	return (add);
+}
