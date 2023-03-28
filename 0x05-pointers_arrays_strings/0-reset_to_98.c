e694804f51228e8931f68a68a2ac6cf1a3ec72bb
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-28 12:26:22 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function for taking a pointer
+ *
+ * @n: integal input
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/READMD.md
@@ -0,0 +1,1 @@
+Readme.md file for the 0x05-pointers_arrays_strings project in alx se programme
