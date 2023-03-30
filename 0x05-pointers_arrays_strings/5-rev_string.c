a9c47b771d6803b4ab0e85974afb831932887308
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-30 07:11:02 -0700

    void rev_string(char *s);

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - funtion to print reverse string
+ * @s: pointer to print string
+ */
+
+void rev_string(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1;j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+}
