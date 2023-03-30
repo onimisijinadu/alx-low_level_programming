7ff234fb3ab27ce453f48d0e59b5db5c5aca0e29
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-30 07:41:21 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - function for printing array
+ * @n: number ofelement
+ * @a: pointer
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("d%", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
