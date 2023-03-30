711eeab5682f4093f66ebf77b7e7b6d20693301b
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-30 07:46:28 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string
+ * @dest: pointer to buffer
+ * @src: string to copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
