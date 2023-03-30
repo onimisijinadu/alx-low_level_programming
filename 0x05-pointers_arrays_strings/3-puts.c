e84346816387480c8fc663b02d692cc75f456669
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-03-30 06:42:58 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "mian.h"
+
+/**
+ * _puts - function to print string
+ *
+ * @str: pointer to print string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
