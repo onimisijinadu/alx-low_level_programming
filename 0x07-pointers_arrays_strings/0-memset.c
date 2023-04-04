925ff43ba7555ded02c968387d37714de04e0b5c
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-04-04 04:25:12 -0700

    More pointers and Array

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - functionto fill the first n bytes
+ * @n: bytes of the memory area pointed to
+ * @s: pointer
+ * @b: constant byte
+ * Return: return a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My Pointer to pointer excersice
