ff92b36b373b90d53b3ab90aa7e85c932135b58b
onimisijinadu <jinaduabdulkareem340@gmail.com>
2023-04-10 06:59:37 -0700

    array

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - function to locate character
+ * @c: first occurence of character
+ * @s: string
+ * Return: "NULL" if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i >= 'a'; i <= 'z'; i++)
+		_putchar(i);
+	return (NULL);
+}
